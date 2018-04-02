cb4b46b9c5b88513029a723eb8029c9b5b401457
Unknown <vominhcong05@gmail.com>
2018-04-02 14:25:55 +0700

    update

--- Models/FGNet.py
+++ Models/FGNet.py
@@ -8,8 +8,6 @@ class FGNet():
     # pass
 
     def __init__(self):
-        self._model = Sequential()
-
         if not os.path.isdir(FGNetConfig.props['MODEL_PATH']):
             os.mkdir(FGNetConfig.props['MODEL_PATH'])
         
@@ -22,35 +20,36 @@ class FGNet():
                                                 monitor='val_loss', 
                                                 verbose=1, 
                                                 save_best_only=True)
-                                                
         self._tensor_board = TensorBoard(log_dir=FGNetConfig.props['LOG_PATH'], 
                                         histogram_freq=0, 
                                         write_graph=True, 
                                         write_images=True)
-
         self._callback_list = [self._model_checkpoint, self._tensor_board]
 
 
     def __reference__(self):
-        self._model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='valid', activation='relu',
+        model = Sequential()
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='valid', activation='relu',
                         input_shape=FGNetConfig.props['INPUT_SHAPE']))
-        self._model.add(AveragePooling2D(pool_size=(2, 2)))
+        model.add(AveragePooling2D(pool_size=(2, 2)))
 
-        self._model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='valid', activation='relu'))
-        self._model.add(AveragePooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='valid', activation='relu'))
+        model.add(AveragePooling2D(pool_size=(2, 2)))
 
-        self._model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='valid', activation='relu'))
-        self._model.add(AveragePooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='valid', activation='relu'))
+        model.add(AveragePooling2D(pool_size=(2, 2)))
 
-        self._model.add(Conv2D(filters=512, kernel_size=(3, 3), padding='valid', activation='relu'))
-        self._model.add(AveragePooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(filters=512, kernel_size=(3, 3), padding='valid', activation='relu'))
+        model.add(AveragePooling2D(pool_size=(2, 2)))
 
-        self._model.add(Flatten())
-        self._model.add(Dense(1024, activation='relu'))
-        self._model.add(Dense(6, activation='softmax'))
+        model.add(Flatten())
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dense(6, activation='softmax'))
+        return model
 
 
     def train(self, X_train, y_train, X_dev, y_dev):
+        self._model = self.__reference__()
         self._model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['loss'])
         self._model.fit(x=X_train, y=y_train, batch_size=FGNetConfig.props['BATCH_SIZE'], 
                                 epochs=FGNetConfig.props['EPOCHS'],
