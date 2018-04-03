d661209fc7f3db9f4383a79a564b809d135c9e37
Unknown <vominhcong05@gmail.com>
2018-04-03 17:36:21 +0700

    ff

--- Models/AGNet.py
+++ Models/AGNet.py
@@ -79,39 +79,39 @@ class AGNet():
 
         model = Sequential()
         model.add(ZeroPadding2D((1, 1), input_shape=(img_rows, img_cols, channel)))
-        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=512, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=512, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=512, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=512, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=512, kernel_size=(3, 3), activation='relu'))
         model.add(ZeroPadding2D((1, 1)))
-        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(Conv2D(filters=512, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
         # Loads ImageNet pre-trained data
@@ -126,7 +126,6 @@ class AGNet():
         # model.add(Dropout(0.5))
         # model.add(Dense(1000, activation='softmax'))
         
-
         model.layers.pop()
         model.outputs = [model.layers[-1].output]
         model.layers[-1].outbound_nodes = []
