a681623840b768dc15203302b0d9b7559a839ead
Unknown <vominhcong05@gmail.com>
2018-04-09 22:41:02 +0700

    f

--- Models/AGNet.py
+++ Models/AGNet.py
@@ -55,9 +55,9 @@ class AGNet():
         model.add(Dropout(0.2))
         model.add(AveragePooling2D(pool_size=(2, 2)))
         
-        # model.add(Conv2D(filters=2048, kernel_size=(3, 3), padding='same', activation='relu'))
-        # model.add(Dropout(0.2))
-        # model.add(AveragePooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(filters=2048, kernel_size=(3, 3), padding='same', activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(AveragePooling2D(pool_size=(2, 2)))
         
         model.add(Flatten())
         model.add(Dense(1024, activation='relu'))
