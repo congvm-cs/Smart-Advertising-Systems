f0334a99c29706a4d116ef08d585996ee9d8e85b
Unknown <vominhcong05@gmail.com>
2018-04-16 16:50:37 +0700

    FFF

--- Models/AGNet.py
+++ Models/AGNet.py
@@ -169,10 +169,15 @@ class AGNet():
 
     def _multi_labels_accuracy(self, y_true, y_pred):
         acc = K.equal(y_true, y_pred)
-        acc = (K.all(acc, axis=-1) == 1)
+        acc = (K.all(acc, axis=-1))
         acc = K.cast(acc, tf.float32)
         return K.mean(acc)
-    
+    # acc = tf.equal(y, tf.round(pred))
+    # acc = (tf.reduce_all(acc, axis=-1))
+    # acc = tf.cast(acc, tf.float32)
+    # acc = tf.reduce_mean(acc)
+
+
     
     def init(self):
         self._model = self.__vgg16_model__()
--- Models/test_mam_thui.py
+++ Models/test_mam_thui.py
@@ -1,17 +1,27 @@
 import numpy as K
 import tensorflow as tf
+
+
 def multi_labels_accuracy(y_true, y_pred):
     acc = K.equal(y_true, y_pred)
     return K.mean(K.all(acc, axis=-1) == True)
     # return acc
 
+def _multi_labels_accuracy(y_true, y_pred):
+    acc = tf.equal(y_true, tf.round(y_pred))
+    acc = (tf.reduce_all(acc, axis=-1) == True)
+    acc = tf.cast(acc, tf.float32)
+    return K.mean(acc)
+
 y = [[1, 0, 1, 0], [0, 0, 1, 1], [0, 0, 1, 1]]
 pred = [[1, 0, 1, 1], [0, 0, 1, 1], [0, 0, 1, 1]]
 
-print(multi_labels_accuracy(y, pred))
+# print(multi_labels_accuracy(y, pred))
 
 x = [[1, 1, 1, 1], 
     [1, 1, 1, 1]]
 
 # print(K.all(x, axis=-1) == 1)
 
+with tf.Session() as sess:
+    print(sess.run(_multi_labels_accuracy(y, pred)))
