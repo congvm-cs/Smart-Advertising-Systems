3ff170052b1422e3f58bc6a7490b5aa4528c64e0
vominhcong05@gmail.com <vominhcong05@gmail.com>
2018-05-29 09:45:19 +0700

    fix Client

--- Clients/Client.py
+++ Clients/Client.py
@@ -3,26 +3,31 @@ sys.path.append('..')
 
 import requests 
 from cv2 import VideoCapture, resize
+import cv2
 import base64 
 import json
 import time
-from src.config import config
-from threading import Thread
 
+import config
+from threading import Thread
 
 class Client():
 
     def __init__(self):
-        self._cap = VideoCapture(0)
+        self._cap = VideoCapture(-1)
+	#self._cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
+	#self._cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
         self._information = {}
         self._stop = False
 
 
     def __get_frame(self):
+	print('hi')
         while not self._stop:
             self._information.clear()
             _, image = self._cap.read()
-            image = resize(image, (0, 0), fx=1, fy=1)
+	    
+            image = resize(image, (0, 0), fx=0.7, fy=0.7)	
 
             print(image.shape)
             image_encoded = base64.b64encode(image)
@@ -43,6 +48,6 @@ class Client():
     def start(self):
         print('Start getting frame!')
 		# start a thread to read frames from the file video stream
-        t = Thread(target=self.__get_frame, args=())
+        t = Thread(target=self.__get_frame, args=[])
         t.daemon = True
         t.start()
